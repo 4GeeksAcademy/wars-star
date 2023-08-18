81f183d369b2052fed8a367ea246d0737a9c203f
Agustin Rolando <138535452+Agustin144@users.noreply.github.com>
2023-08-18 20:22:36 +0000

    navbar a medias

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
+			<img id="local-nav-logo-desktop"  className="d-flex ms-5 " width= "100" src="https://logos-marcas.com/wp-content/uploads/2020/11/Star-Wars-Logo.png"   />
+			
+          <a className=" dropdown-toggle btn btn-primary me-4" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Favorites
+          </a>
+          <ul className="dropdown-menu">
+            
+          </ul>
 		</nav>
 	);
 };
