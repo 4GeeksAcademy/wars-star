b85a62add5fd97f7b46852033e3446c1c780bb19
Cecilia <45336766+ceciliadean182@users.noreply.github.com>
2023-08-21 12:31:19 +0000

    Toques finales

--- src/js/component/characters.js
+++ src/js/component/characters.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Characters = () => {
     return (
@@ -7,10 +8,10 @@ const Characters = () => {
                 <img src="https://img.particlenews.com/img/id/2WM95P_0n0JJ8NK00?type=thumbnail_1024x512" class="card-img-top mx-0" alt="..."/>
                 <div class="card-body">
                     <h5 class="card-title">Luke Skywalker</h5>
-                    <p class="card-text">Gender: <span>male</span></p>
-                    <p class="card-text" id="p-datos">Hair Color: <span>blonde</span></p>
+                    <p class="card-text my-0">Gender: <span>male</span></p>
+                    <p class="card-text my-0" id="p-datos">Hair Color: <span>blonde</span></p>
                     <p class="card-text">Eye Color: <span>blue</span></p>
-                    <a href="#" class="btn btn-primary">Learn More!</a><button className="btn btn-warning position-absolute bottom-0 end-0 my-3 mx-3"><i class="fa fa-heart"></i></button>
+                    <Link to="#" class="btn btn-outline-primary">Learn More!</Link><button className="btn btn-outline-warning position-absolute bottom-0 end-0 my-3 mx-3"><i class="fa fa-heart"></i></button>
                 </div>
             </div>
         </div>
