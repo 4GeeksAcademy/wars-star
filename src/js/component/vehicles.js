3ff38361e061f1814047f8bbf17e82a8a7bc9607
claudiasgithub2 <138537469+claudiasgithub2@users.noreply.github.com>
2023-08-21 15:19:46 +0000

    vehiculos

--- src/js/component/vehicles.js
+++ src/js/component/vehicles.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useEffect, useContext } from "react"
+import { Context } from "../store/appContext"
+import Planets from "./planets"
 
 
 
-const  Vehicles = () => {
-    return(
+const Vehicles = () => {
+    const { store, actions } = useContext(Context)
 
-        <div className="card" style={{width: "18rem"}}>
+    useEffect(() => {
+        actions.obtenerVehiculosClaudia();
+    }, []);
+
+
+    return (
+
+        <div className="card" style={{ width: "18rem" }}>
             <h1 className="card-title text-left text-danger"> Vehicles</h1>
-        <img src="https://files.cults3d.com/uploaders/4434823/illustration-file/1450688883-1419-6018/Capture_d_e_cran_2015-12-21_a__10.03.30.png" className="card-img-top img-thumbnail"  style={{width: "400" }}alt="..."/>
-        <div className="card-body">
-            <h5 className="card-title text-left text-black">Naves</h5>
-            <p className="card-text">Marca</p>
-            <a href="#" className="btn btn-outline-primary">Learn more!</a>
-            <button type="button" className="btn btn-outline-warning position-absolute bottom-0 end-0 my-3 me-2"><i class="fa fa-heart"></i></button>
-        </div>
+            <img src="https://files.cults3d.com/uploaders/4434823/illustration-file/1450688883-1419-6018/Capture_d_e_cran_2015-12-21_a__10.03.30.png" className="card-img-top img-thumbnail" style={{ width: "400" }} alt="..." />
+            <div className="card-body">
+                {store.vehicles.map((item, index) =>
+
+                    <Vehicles
+                        name={item.name}
+                        model={item.model}
+                        passengers={item.passengers}
+                        id={index}
+                    />
+
+                )}
+                <h5 className="card-title text-left text-black">Naves</h5>
+                <p className="card-text">Marca</p>
+                <a href="#" className="btn btn-outline-primary">Learn more!</a>
+                <button type="button" className="btn btn-outline-warning position-absolute bottom-0 end-0 my-3 me-2"><i class="fa fa-heart"></i></button>
+            </div>
         </div>
 
     )
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,14 +3,15 @@ import React from "react";
 import "../../styles/home.css";
 
 import { Navbar } from "../component/navbar";
-import Planets from "../components/plantes";
+import Planets from "../component/planets";
 import Vehicles from "../component/vehicles";
 import Characters from "../component/characters";
 
 export const Home = () => (
 	<div className="mt-5">
-		<Planets/>
+		<Planets />
 		<Characters />
-    <Vehicles />
+		<Vehicles />
+
 	</div>
 )
