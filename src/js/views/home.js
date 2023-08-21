bf42d747e8d63ace014f4b6934dc7fe288d30c8b
Luica <138072443+lumartins311@users.noreply.github.com>
2023-08-21 14:55:10 +0000

    planetassss

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,14 +3,60 @@ import React from "react";
 import "../../styles/home.css";
 
 import { Navbar } from "../component/navbar";
-import Planets from "../components/plantes";
+import Planets from "../component/planets";
 import Vehicles from "../component/vehicles";
 import Characters from "../component/characters";
 
-export const Home = () => (
+export const Home = () => {
+
+	const { store, actions } = useContext(Context);
+
+
+	const [state, setState] = useState({
+
+	});
+	useEffect(() => {
+		actions.obtenerplanetas();
+	}, []);
+	return (
+
 	<div className="mt-5">
-		<Planets/>
-		<Characters />
-    <Vehicles />
+		<ul className=" m-0 p-0 d-flex flex-wrap horizontal-scrollable" id="contact-list"  >
+			{store.Planets.map((item, index) => (
+				<Planets
+					name={item.name}
+					population={item.population}
+					terrain={item.terrain}
+					id={index}
+
+
+
+				/>
+
+			))}
+		</ul>
+
+		<Characters
+
+
+
+
+
+
+
+
+
+		/>
+		<Vehicles
+
+
+
+
+
+
+
+
+
+		/>
 	</div>
-)
+)}
