cffeacdc169f20fbf362b439171ab6aa095d2fca
Cristian Díaz <88348311+cristiandiazperdomo@users.noreply.github.com>
2023-08-22 13:21:45 +0000

    cambios

--- src/js/views/Details.js
+++ src/js/views/Details.js
@@ -10,6 +10,7 @@ export const Details = () => {
 
     const { id } = useParams();
     const pathName = useLocation().pathname;
+    const images = ["https://lumiere-a.akamaihd.net/v1/images/sandcrawler-bio-1_11fee365.jpeg?region=342%2C73%2C938%2C474", "https://static.wikia.nocookie.net/esstarwars/images/f/f2/T-16_skyhopper_-_SW_20.png/revision/latest?cb=20181201201037", "https://static.wikia.nocookie.net/starwars/images/7/7b/MarkIIMediumBlaster-SWL36.jpg/revision/latest?cb=20220102162324", "https://static.wikia.nocookie.net/starwars/images/e/e2/TIE-Fighter-RO-SWCT.png/revision/latest/scale-to-width-down/705?cb=20170506062919", "https://static.wikia.nocookie.net/starwars/images/0/0d/Snowspeeder.png/revision/latest?cb=20161110010008", "https://static.wikia.nocookie.net/esstarwars/images/1/17/TIE_Bomber_BF2.png/revision/latest?cb=20171101030957", "https://static.wikia.nocookie.net/starwars/images/9/9a/AT-ACT_RO.png/revision/latest?cb=20220915023046", "https://static.wikia.nocookie.net/starwars/images/f/ff/ATST-SWBdice.png/revision/latest?cb=20230723050455", "https://static.wikia.nocookie.net/starwars/images/3/3b/Cloud-car-v2.png/revision/latest?cb=20160617064429", "https://static.wikia.nocookie.net/esstarwars/images/4/42/Eclipsesb.JPG/revision/latest?cb=20120527170232"]
 
     useEffect(() => {
         if (pathName.includes("characters")) {
@@ -34,6 +35,7 @@ export const Details = () => {
             actions.getDetails("vehicles", id).then(() => {
                 setInfo(store.details);
                 setTypeOfDetails("vehicles");
+                setImg(images[id - 1]);
 
             });
         }
