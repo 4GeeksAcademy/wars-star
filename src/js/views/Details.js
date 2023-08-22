fa12f30a009050c02d1b6d788a40cc4766956f58
Cristian Díaz <88348311+cristiandiazperdomo@users.noreply.github.com>
2023-08-22 13:19:37 +0000

    cambios

--- src/js/views/Details.js
+++ src/js/views/Details.js
@@ -4,6 +4,7 @@ import { useParams, useLocation } from 'react-router-dom';
 
 export const Details = () => {
     const { store, actions } = useContext(Context);
+    const [typeOfDetails, setTypeOfDetails] = useState();
     const [img, setImg] = useState("");
     const [info, setInfo] = useState({});
 
@@ -15,17 +16,24 @@ export const Details = () => {
             actions.getDetails("characters", id).then(() => {
                 setInfo(store.details);
                 setImg("https://starwars-visualguide.com/assets/img/characters/" + id + ".jpg");
+                setTypeOfDetails("characters");
             })
         };
         if (pathName.includes("planets")) {
             actions.getDetails("planets", id).then(() => {
                 setInfo(store.details);
+                console.log(id);
                 setImg("https://starwars-visualguide.com/assets/img/planets/" + id + ".jpg");
+
+
+
+                setTypeOfDetails("planets");
             });
         }
         if (pathName.includes("vehicles")) {
             actions.getDetails("vehicles", id).then(() => {
                 setInfo(store.details);
+                setTypeOfDetails("vehicles");
 
             });
         }
@@ -42,26 +50,99 @@ export const Details = () => {
             </div>
             <div className="border-top h-10 my-4 border-danger"></div>
             <div className="d-flex justify-content-between text-danger text-center">
-                <div>
-                    <p>Name</p>
-                    <p>{info.name}</p>
-                </div>
-                <div>
-                    <p>Birth Year</p>
-                    <p>{info.birth_year}</p>
-                </div>
-                <div>
-                    <p>Height</p>
-                    <p>{info.height}</p>
-                </div>
-                <div>
-                    <p>Skin Color</p>
-                    <p>{info.skin_color}</p>
-                </div>
-                <div>
-                    <p>Eye Color</p>
-                    <p>{info.eye_color}</p>
-                </div>
+                {typeOfDetails === "characters" &&
+                    <>
+                        <div>
+                            <p>Name</p>
+                            <p>{info.name}</p>
+                        </div>
+                        <div>
+                            <p>Birth Year</p>
+                            <p>{
+                                info.birth_year
+                            }</p>
+                        </div>
+                        <div>
+                            <p>Height</p>
+                            <p>{
+                                info.height
+                            }</p>
+                        </div>
+                        <div>
+                            <p>Skin Color</p>
+                            <p>{
+                                info.skin_color
+                            }</p>
+                        </div>
+                        <div>
+                            <p>Eye Color</p>
+                            <p>{
+                                info.eye_color
+                            }</p>
+                        </div>
+                    </>}
+                {typeOfDetails === "vehicles" &&
+                    <>
+                        <div>
+                            <p>Name</p>
+                            <p>{info.name}</p>
+                        </div>
+                        <div>
+                            <p>Model</p>
+                            <p>{
+                                info.model
+                            }</p>
+                        </div>
+                        <div>
+                            <p>Length</p>
+                            <p>{
+                                info.length
+                            }</p>
+                        </div>
+                        <div>
+                            <p>Cost in credits</p>
+                            <p>{
+                                info.cost_in_credits
+                            }</p>
+                        </div>
+                        <div>
+                            <p>Consumables</p>
+                            <p>{
+                                info.consumables
+                            }</p>
+                        </div>
+                    </>}
+                {typeOfDetails === "planets" &&
+                    <>
+                        <div>
+                            <p>Name</p>
+                            <p>{info.name}</p>
+                        </div>
+                        <div>
+                            <p>Created</p>
+                            <p>{
+                                info.created
+                            }</p>
+                        </div>
+                        <div>
+                            <p>Diameter</p>
+                            <p>{
+                                info.diameter
+                            }</p>
+                        </div>
+                        <div>
+                            <p>Climate</p>
+                            <p>{
+                                info.climate
+                            }</p>
+                        </div>
+                        <div>
+                            <p>Terrain</p>
+                            <p>{
+                                info.terrain
+                            }</p>
+                        </div>
+                    </>}
             </div>
         </div>
     );
